eb3c768d9c5285c9dbb90711bde8a42904ee79c2
wiggle1000 <austintempletonthepawner@yahoo.com>
2026-03-06 14:52:06 -0600

    Update socials.js

--- components/socials.js
+++ components/socials.js
@@ -9,7 +9,7 @@ class Socials extends HTMLElement {
         		<h1>Find us here!</h1>
 				<ul>
 					<li><a href="https://discord.gg/tSTEm5U2Eb"><img src="/assets/socials-discord.png">		<p>Discord</p></a></li>
-					<li><a href=""><img src="/assets/socials-meetup.png">		<p>Meetup</p></a></li>
+					<li><a href="https://www.meetup.com/hsv-cyberspace"><img src="/assets/socials-meetup.png">		<p>Meetup</p></a></li>
 					<li><a href=""><img src="/assets/socials-facebook.png">		<p>Facebook</p></a></li>
 					<li><a href=""><img src="/assets/socials-web.png">			<p>Web</p></a></li>
 				</ul>
